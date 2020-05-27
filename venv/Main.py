599137e9e8109ee51dc981cdbf775603d8b72adc
darkllen <yankiniigor@gmail.com>
2020-05-27 12:19:33 +0300

    hide credentials

--- venv/Main.py
+++ venv/Main.py
@@ -11,10 +11,12 @@ data_challenge =  {
     "categories":[],
     "creator_id": "asdsaddsa"
 }
+
+
 site = 'https://us-central1-challengeup-49057.cloudfunctions.net/add_user'
 site2 = 'https://us-central1-challengeup-49057.cloudfunctions.net/add_challenge'
 add_user = "http://127.0.0.1:5000/add_user"
 add_challenge = "http://127.0.0.1:5000/add_challenge"
 
-r = requests.post(site2, json=data_challenge)
+r = requests.post(add_challenge, json=data_challenge)
 print (r.json())
